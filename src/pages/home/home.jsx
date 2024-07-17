db01de5c6534f48e852ac041d97a2ab6a0ec4af1
Olena Yastrebtsova <uaalena202@gmail.com>
2024-07-17 13:09:38 +0300

    nc

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,33 @@
+import styles from "../../style";
+import Hero from "../../components/hero/Hero";
+import New from "../../components/new/New";
+import Promotion from '../../components/promotion/Promotion';
+import Bestsellers from "../../components/bestsellers/Bestsellers";
+import AboutUsText from "../../components/hero/AboutUs";
+
+const Home = () => {
+    return <div>
+        
+        {/* hero */}
+        <div className={`${styles.paddingX} ${styles.paddingY} ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Hero />
+            </div>
+        </div>
+        <AboutUsText />
+        {/* hero end */}
+
+
+        {/* main */}
+        <div className={`${styles.paddingX} ${styles.flexStart} flex grow`} >
+            <div className={`${styles.boxWidth}`}>
+                <New />
+                <Promotion />
+                <Bestsellers />
+            </div>
+        </div>
+    </div>
+}
+
+export default Home;
+
